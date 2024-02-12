7842bdaaea598425e5bf026a6cba3573b9e03f0a
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-02-12 17:33:51 -0600

    changed the config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,67 +3,69 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    main: "./src/js/index.js",
-    install: "./src/js/install.js",
-    database: "./src/js/database.js",
-    editor: "./src/js/editor.js",
-    header: "./src/js/header.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Jate App",
-      template: "./index.html",
-    }),
-    new InjectManifest({
-      swSrc: "./src-sw.js",
-      swDest: "sw.js",
-    }),
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: true,
-      name: "Just Another Text Editor",
-      short_name: "J.A.T.E.",
-      description: "A text editor app that you can use.",
-      background_color: "#228B22",
-      theme_color: "#FFA500",
-      start_url: "/",
-      publicPath: "/",
-      icons: [
+module.exports = () => {
+  return {
+    mode: "development",
+    entry: {
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      database: "./src/js/database.js",
+      editor: "./src/js/editor.js",
+      header: "./src/js/header.js",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Jate App",
+        template: "./index.html",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E.",
+        description: "A text editor app that you can use.",
+        background_color: "#228B22",
+        theme_color: "#FFA500",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
+    module: {
+      rules: [
         {
-          src: path.resolve("src/images/logo.png"),
-          sizes: [96, 128, 192, 256],
-          destination: path.join("assets", "icons"),
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
-      ],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: [
-              "@babel/plugin-proposal-object-rest-spread",
-              "@babel/transform-runtime",
-            ],
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
